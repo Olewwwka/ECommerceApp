067175a97f62f2fd5fc4f989e14771774df8ba51
Olewwwka <netvinkakk@gmail.com>
2025-03-10 17:38:36 +0300

    extacted

---
+++ ECommerce.Core/Abstractions/RepostoriesInterfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Abstractions.RepostoriesInterfaces
+{
+    public interface IUnitOfWork
+    {
+        IUsersRepository UsersRepository { get; }
+        void Dispose();
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ ECommerce.Core/Abstractions/RepostoriesInterfaces/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Abstractions.RepostoriesInterfaces
+{
+    public interface IUsersRepository
+    {
+        Task<List<UserEntity>> GetAllAsync();
+        Task AddAsync(UserEntity user);
+        Task<UserEntity> GetByEmailAsync(string email);
+    }
+}
