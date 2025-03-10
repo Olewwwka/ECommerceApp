096be715cb2a8fc56d668ce8e30a4cf1e0afd500
Olewwwka <netvinkakk@gmail.com>
2025-03-10 17:39:12 +0300

    creating repositoris

---
+++ ECommerce.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Core.Abstractions.RepostoriesInterfaces;
+using ECommerce.Infrastructure.Persistence.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ECommerceDbContext _context;
+        public IUsersRepository UsersRepository { get; }
+        public UnitOfWork(ECommerceDbContext context)
+        {
+            _context = context;
+            UsersRepository = new UsersRepository(context);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ ECommerce.Infrastructure/Persistence/Repositories/UsersRepository.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Core.Abstractions.RepostoriesInterfaces;
+using ECommerce.Core.Entities;
+using ECommerce.Infrastructure.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistence.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly ECommerceDbContext _context;
+        public UsersRepository(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(UserEntity user)
+        {
+            var shoppingCart = new ShoppingCartEntity
+            {
+                User = user,
+                Products = new List<ProductEntity>()
+            };
+
+            await _context.ShoppingCarts.AddAsync(shoppingCart);
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<UserEntity>> GetAllAsync()
+        {
+            return await _context.Users.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<UserEntity> GetByEmailAsync(string email)
+        {
+            var userEntity = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Email == email);
+            return userEntity;
+        }
+
+    }
+}
