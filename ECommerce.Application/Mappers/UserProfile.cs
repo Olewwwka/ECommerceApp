ab7d9e951c891bd8f2c9bce90011c7868d6d28f6
Olewwwka <netvinkakk@gmail.com>
2025-03-10 17:39:49 +0300

    create user/userentity mapper

---
+++ ECommerce.Application/Mappers/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ECommerce.Core.Entities;
+using ECommerce.Core.Models;
+
+namespace ECommerce.Application.Mappers
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserEntity>()
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash));
+
+            CreateMap<UserEntity, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash));
+        }
+    }
+}
---
+++ ECommerce.Core/Abstractions/ServicesInterfaces/IJWTProvider.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Abstractions.ServicesInterfaces
+{
+    public interface IJwtProvider
+    {
+        string GenerateToken(User user);
+    }
+}
