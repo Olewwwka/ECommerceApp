34f3d63f907e162f612f1a6f514d87b382eecbbd
Olewwwka <netvinkakk@gmail.com>
2025-03-10 16:31:14 +0300

    Creating user model

---
+++ ECommerce.Core/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Models
+{
+    public class User
+    {
+        public string Login { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public User(string login, string passwordHash, string email, string firstname, string lastname)
+        {
+            Login = login;
+            PasswordHash = passwordHash;
+            Email = email;
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public static User Create(string login, string passwordHash, string email, string firstname, string lastname)
+        {
+            return new User(login, passwordHash, email, firstname, lastname);
+        }
+    }
+}
