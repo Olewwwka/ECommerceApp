e33c38b3356c91b5834e40894c2aafe97765a9fe
Olewwwka <netvinkakk@gmail.com>
2025-03-12 19:42:05 +0300

    use configurations

--- ECommerce.Infrastructure/Persistence/Configuration/ECommerceDbContext.cs
+++ ECommerce.Infrastructure/Persistence/Configuration/ECommerceDbContext.cs
@@ -2,100 +2,27 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ECommerce.Core.Entities;
+using Microsoft.Extensions.Options;
+using ECommerce.Infrastructure.Configurations;
 
 namespace ECommerce.Infrastructure.Persistence.Configuration
 {
-    public class ECommerceDbContext : DbContext
+    public class ECommerceDbContext(DbContextOptions<ECommerceDbContext> options,
+        IOptions<AuthorizationOptions> authOptions) : DbContext(options)
     {
         private readonly IConfiguration _configuration;
         public DbSet<UserEntity> Users { get; set; }
-       /* public DbSet<CategoryEntity> Categories { get; set; }
-        public DbSet<OrderEntity> Orders { get; set; }
-        public DbSet<OrderItemEntity> OrderItems { get; set; }
-        public DbSet<ReviewEntity> Reviews { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
         public DbSet<ProductEntity> Products { get; set; }
-        public DbSet<ShoppingCartEntity> ShoppingCarts { get; set; }
-        public DbSet<AuditLogsEntity> AuditLogs { get; set; }*/
-        public ECommerceDbContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
+        public DbSet<RoleEntity> Roles { get; set; }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<UserEntity>(userBuilder =>
-            {
-                userBuilder.ToTable("Users").HasKey(x => x.UserId);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ECommerceDbContext).Assembly);
 
-                /*userBuilder.HasMany<OrderEntity>()
-                    .WithOne()
-                    .HasForeignKey(x => x.UserId)
-                    .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.ApplyConfiguration(new RolePermissionConfiguration(authOptions.Value));
 
-                userBuilder.HasMany<ReviewEntity>()
-                    .WithOne()
-                    .HasForeignKey(x => x.UserId)
-                    .OnDelete(DeleteBehavior.Cascade);
-
-                userBuilder.HasMany<AuditLogsEntity>()
-                    .WithOne(log => log.User)
-                    .HasForeignKey(log => log.UserId)
-                    .OnDelete(DeleteBehavior.Cascade);
-
-                userBuilder.HasOne(user => user.ShoppingCart)
-                    .WithOne(cart => cart.User)
-                    .HasForeignKey<ShoppingCartEntity>(cart => cart.UserId)
-                    .OnDelete(DeleteBehavior.Cascade);*/
-            });
-
-            /*modelBuilder.Entity<ShoppingCartEntity>(cartBuilder =>
-            {
-                cartBuilder.ToTable("ShoppingCarts").HasKey(sc => sc.CartId);
-
-                cartBuilder.HasOne(sc => sc.User)
-                           .WithOne()
-                           .HasForeignKey<ShoppingCartEntity>(sc => sc.UserId);
-            });
-
-            modelBuilder.Entity<OrderEntity>(orderBuilder =>
-            {
-                orderBuilder.ToTable("Orders").HasKey(o => o.UserId);
-
-                orderBuilder.HasOne<UserEntity>()
-                            .WithMany()
-                            .HasForeignKey(o => o.UserId)
-                            .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<AuditLogsEntity>(logBuilder =>
-            {
-                logBuilder.ToTable("AuditLogs").HasKey(log => log.LogId);
-
-                logBuilder.HasOne(log => log.User)
-                          .WithMany()
-                          .HasForeignKey(log => log.UserId)
-                          .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<ReviewEntity>(reviewBuilder =>
-            {
-                reviewBuilder.ToTable("Reviews").HasKey(r => r.ReviewId);
-
-                reviewBuilder.HasOne(r => r.User)
-                             .WithMany()
-                             .HasForeignKey(r => r.UserId)
-                             .OnDelete(DeleteBehavior.Cascade);
-            });*/
+            Console.WriteLine($"Loaded RolePermissions: {authOptions.Value}");
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("ECommerceDbContext"))
-                .UseLoggerFactory(CreateLoggerFactory())
-                .EnableSensitiveDataLogging();
-        }
-
-        public ILoggerFactory CreateLoggerFactory() =>
-            LoggerFactory.Create(builder => { });
     }
 }
