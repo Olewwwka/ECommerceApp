6eeef5ab9cf2cf26de4a338fe5c4a74ed5d544b6
Olewwwka <netvinkakk@gmail.com>
2025-03-12 12:45:58 +0300

    auth scheme

---
+++ ECommerce.API/Extensions/ApiExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.API.Endpoints;
+
+namespace ECommerce.API.Extensions
+{
+    public static class ApiExtensions
+    {
+        public static void AddMappedEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapUsersEndpoints();
+        }
+
+        public static void AddApiAutorization(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddAuthorization()
+        }
+    }
+}
