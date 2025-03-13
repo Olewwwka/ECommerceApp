e0bbb5d15bfa35e1ecd421000c0ffdd9e559d8ff
Olewwwka <netvinkakk@gmail.com>
2025-03-13 21:35:04 +0300

    create models

---
+++ ECommerce.Core/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Models
+{
+    public class Category
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ ECommerce.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; } = 0;
+        public int StockQuantity { get; set; } = 0;
+        public Product(int categoryId, string name, string description, decimal price, int stockQuantity)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Price = price;
+            StockQuantity = stockQuantity;
+        }
+    }
+}
