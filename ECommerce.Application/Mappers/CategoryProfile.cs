34602d5f015338a2a4e157f674a8662dee478306
Olewwwka <netvinkakk@gmail.com>
2025-03-13 21:35:42 +0300

    create mapper profiles

---
+++ ECommerce.Application/Mappers/CategoryProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ECommerce.Core.Entities;
+using ECommerce.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Mappers
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryEntity>()
+                .ForMember(dest => dest.CategoryId, opt => opt.Ignore())
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+
+            CreateMap<CategoryEntity, Category>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+        }
+    }
+}
---
+++ ECommerce.Application/Mappers/ProductProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ECommerce.Core.Entities;
+using ECommerce.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Mappers
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductEntity>()
+                .ForMember(dest => dest.ProductId, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId)); 
+
+            CreateMap<ProductEntity, Product>()
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Category.CategoryId));
+        }
+    }
+}
